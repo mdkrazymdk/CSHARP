5574f9bec8bcef9b8bd80c814221b47d0775286b
mdkrazymdk <mdkrazymdk@gmail.com>
2022-09-20 21:53:53 +0300

    LAB2

---
+++ lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn;
+            int nk;
+            int k;
+            double res = 0;
+            nn = int.Parse(Console.ReadLine());
+            nk = int.Parse(Console.ReadLine());
+
+            for (k = nn;  k<nk; k++)
+            {
+                if (0 <= nn & nn <= nk)
+                {
+                    res += (Math.Pow(k, 2) - (Math.Pow(-1, k + 1) * (Math.Pow(k, 3)))) / ((Math.Pow(k, 2)) + k + 1);
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
