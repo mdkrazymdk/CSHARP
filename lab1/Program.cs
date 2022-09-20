9b8adca6a17aec97cdfa88fc67c94e8a02d273c6
mdkrazymdk <mdkrazymdk@gmail.com>
2022-09-20 22:03:54 +0300

    LAB1

---
+++ lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R;
+            double I;
+            double U;
+            Console.WriteLine("Введiть силу струму");
+            I = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть напругу");
+            U = double.Parse(Console.ReadLine());
+            if (U > 0)
+            { 
+                Console.WriteLine("емпiрична формула справедлива для будь - якої дiлянки ланцюга: R=I/U");
+            
+                R = I / U;
+                Console.WriteLine(R);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
